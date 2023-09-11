9b7179acf3d341781f8b631c13b4813ca7b8c0bf
jtbry <justintbry@gmail.com>
2023-09-11 15:41:26 -0400

    feat: take user input and utilize SandwichMachine

--- main.py
+++ main.py
@@ -85,3 +85,34 @@ class SandwichMachine:
 
 
 ### Make an instance of SandwichMachine class and write the rest of the codes ###
+choice = ""
+sandwich_sizes = ["small", "medium", "large"]
+sandwich_maker = SandwichMachine(resources)
+while choice != "off":
+    choice = input("What would you like? (small/medium/large/off/report): ")
+    if choice in sandwich_sizes:
+        # If it is a valid sandwich size select the recipe
+        recipe = recipes[choice]
+        if sandwich_maker.check_resources(recipe["ingredients"]):
+            # If we have enough resources process coins
+            coins = sandwich_maker.process_coins()
+            if sandwich_maker.transaction_result(coins, recipe["cost"]):
+                # If we are given enough money make the sandwich
+                sandwich_maker.make_sandwich(choice, recipe["ingredients"])
+                print(f"{choice} sandwich is ready. Bon appetit!")
+            else:
+                # If we are not given enough money, do nothing
+                print("Sorry that's not enough money. Money refunded.")
+        else:
+            # If there is not enough resources, do nothing
+            print(f"Not enough ingredients to make a {choice} sandwich.")
+    elif choice == "off":
+        # If the user wants to turn off the machine, break the loop
+        break
+    elif choice == "report":
+        # If the user wants to see the report, print the resources
+        for k in sandwich_maker.machine_resources:
+            print(f"{k}: {resources[k]}")
+    else:
+        # If the user enters something else, do nothing
+        print("Sorry, I don't understand that.")
