3fe0933cf3929fb8e633da751b67f3e9b3b27919
Justin Bryant <justintbry@gmail.com>
2023-09-19 14:26:15 -0400

    feat(assignment2): add code to sandwich_maker.py

--- assignment2/sandwich_maker.py
+++ assignment2/sandwich_maker.py
@@ -5,7 +5,17 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for k in ingredients:
+            # Iterate all ingredients
+            if self.machine_resources[k] < ingredients[k]:
+                # If any resource is less than the required amount, return False
+                print(f"Sorry, there is not enough {k}.")
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        """Deduct the required ingredients from the resources.
+           Hint: no output"""
+        for k in order_ingredients:
+            # Iterate order ingredients and deduct resources
+            self.machine_resources[k] -= order_ingredients[k]
